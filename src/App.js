3cae57838297e6e04d1f2fbf723985c5be042325
saksaikot <saksaikot@gmail.com>
2021-03-12 21:38:50 +0600

    converted App component function to class

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import "./App.css";
+import React, { Component } from "react";
 
 import Person from "./components/Person";
-function App() {
-  return (
-    <div className="App">
-      <h3>Hello React</h3>
-      <Person name="Saikot" age="30"></Person>
-      <Person name="Sagor" age="26"></Person>
-      <Person name="Shawon" age="23"></Person>
-      <Person name="Abir" age="27"></Person>
-    </div>
-  );
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h3>Hello React</h3>
+        <Person name="Saikot" age="30"></Person>
+        <Person name="Sagor" age="26"></Person>
+        <Person name="Shawon" age="23"></Person>
+        <Person name="Abir" age="27"></Person>
+      </div>
+    );
+  }
 }
 
 export default App;
