366d1ee17996c36573de1b2943459568c9132081
saksaikot <saksaikot@gmail.com>
2021-03-12 22:27:34 +0600

    added css file to book component

--- src/components/Book.jsx
+++ src/components/Book.jsx
@@ -1,8 +1,8 @@
-
+import "../css/Book.css";
 const Book=props=>{
   const {name,writer}=props.book;
   return(
-    <div>
+    <div className="book">
       <h4>Book: {name}</h4>
       <p>Writer: {writer}</p>
       <input type="text" name="" id="" onKeyUp={(e)=>props.event(e,props.book)} placeholder="Change Book Name"/>
